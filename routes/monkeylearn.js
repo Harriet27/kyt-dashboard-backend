a1e4adb71b4709f84f45d4b6a7a21c0999fb164f
Harriet27 <aldrich.neil.hung@gmail.com>
2022-09-14 11:39:43 +0700

    correct indentation for monkeylearn file

--- routes/monkeylearn.js
+++ routes/monkeylearn.js
@@ -3,26 +3,26 @@ const axios = require('axios');
 const router = express.Router();
 
 router.post('/classify', async (req, res, next) => {
-    const body = {
-        data: req.body.data,
-    };
-    const options = {
-        headers: {
-            "Authorization": `Token ${process.env.MONKEYLEARN_API_KEY}`,
-        },
-    };
-    try {
-        const response = await axios.post(
-            `https://api.monkeylearn.com/v3/classifiers/${process.env.MONKEYLEARN_MODEL_ID_SA}/classify/`,
-            body,
-            options
-        );
-        return res.status(200).json({
-            data: response.data,
-        });
-    } catch (err) {
-        return res.status(500).send(err);
-    }
+  const body = {
+    data: req.body.data,
+  };
+  const options = {
+    headers: {
+      "Authorization": `Token ${process.env.MONKEYLEARN_API_KEY}`,
+    },
+  };
+  try {
+    const response = await axios.post(
+      `https://api.monkeylearn.com/v3/classifiers/${process.env.MONKEYLEARN_MODEL_ID_SA}/classify/`,
+      body,
+      options
+    );
+    return res.status(200).json({
+      data: response.data,
+    });
+  } catch (err) {
+    return res.status(500).send(err);
+  }
 });
 
 module.exports = router;
