9e69269f229fe8c6a29dbc6866a6190d3e7c88f4
Harriet27 <aldrich.neil.hung@gmail.com>
2022-09-14 10:00:24 +0700

    fix request body format

--- routes/monkeylearn.js
+++ routes/monkeylearn.js
@@ -2,23 +2,27 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
-const ml_api_url = process.env.MONKEYLEARN_API_URL;
-const ml_api_key = process.env.MONKEYLEARN_API_KEY;
-const ml_model_id_sa = process.env.MONKEYLEARN_MODEL_ID_SA;
-
 router.post('/classify', async (req, res, next) => {
     const body = {
-        data: [ req.body.text ],
+        data: req.body.data,
     };
     const options = {
         headers: {
-            "Authorization": `Token ${ml_api_key}`,
+            "Authorization": `Token ${process.env.MONKEYLEARN_API_KEY}`,
         },
     };
-    const response = await axios.post(ml_api_url, body, options);
-    return res.status(200).json({
-        data: response.data,
-    });
+    try {
+        const response = await axios.post(
+            `https://api.monkeylearn.com/v3/classifiers/${process.env.MONKEYLEARN_MODEL_ID_SA}/classify/`,
+            body,
+            options
+        );
+        return res.status(200).json({
+            data: response.data,
+        });
+    } catch (err) {
+        return res.status(500).send(err);
+    }
 });
 
 module.exports = router;
