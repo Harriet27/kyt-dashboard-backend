9d244c3bdc54a5ab14eb41fbe44cd7e9b26fc237
Harriet27 <aldrich.neil.hung@gmail.com>
2022-09-29 14:05:16 +0700

    add to router

--- routes/comments.js
+++ routes/comments.js
@@ -5,5 +5,6 @@ router.get('/', Controllers.getAll);
 router.get('/:comment_id', Controllers.getByID);
 router.post('/post/:post_id', Controllers.getPostCommentNew);
 router.post('/create', Controllers.create);
+router.delete('/delete', Controllers.deleteById);
 
 module.exports = router;
